a7e938a51fbc66fc74b76b21f82ba2f9722ac628
André Alvim <andre@alvim.net>
2024-06-29 16:04:16 -0300

    feat: add factory pattern on use-cases

--- src/http/controllers/register.controller.ts
+++ src/http/controllers/register.controller.ts
@@ -1,8 +1,7 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import { z } from "zod";
-import { RegisterUseCase } from "../../use-cases/register.use-case";
-import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users.repository";
 import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists.error";
+import { makeRegisterUseCase } from "@/use-cases/factories/make-register.use-case";
 
 export async function Register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -14,10 +13,13 @@ export async function Register(request: FastifyRequest, reply: FastifyReply) {
   const { name, email, password } = registerBodySchema.parse(request.body);
 
   try {
-    const usersRepository = new PrismaUsersRepository();
-    const registerUseCase = new RegisterUseCase(usersRepository);
+    const registerUseCase = makeRegisterUseCase();
 
-    const { user } = await registerUseCase.execute({ name, email, password });
+    const { user } = await registerUseCase.execute({
+      name,
+      email,
+      password,
+    });
 
     // return user;
   } catch (e) {
